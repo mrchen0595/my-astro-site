c1387173db2e1bec5cd9c80be76be4a26312cc90
mrchen0595 <chenweifeng0595@gmail.com>
2026-08-12 15:09:04 +0800

    Disable CSP in local development

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,32 +1,37 @@
 // @ts-check
+
 import { defineConfig } from "astro/config";
-
 import vercel from "@astrojs/vercel";
-
 import sitemap from "@astrojs/sitemap";
 
-// https://astro.build/config
+const isProduction = process.env.NODE_ENV === "production";
+
 export default defineConfig({
   site: "https://my-astro-site-rose.vercel.app",
+
   adapter: vercel({
     staticHeaders: true,
   }),
-  security: {
-    csp: {
-      directives: [
-        "default-src 'self'",
-        "img-src 'self' data:",
-        "font-src 'self' https://fonts.gstatic.com",
-        "connect-src 'self'",
-        "object-src 'none'",
-        "base-uri 'self'",
-        "form-action 'self'",
-      ],
 
-      styleDirective: {
-        resources: ["'self'", "https://fonts.googleapis.com"],
-      },
-    },
+  security: {
+    csp: isProduction
+      ? {
+          directives: [
+            "default-src 'self'",
+            "img-src 'self' data:",
+            "font-src 'self' https://fonts.gstatic.com",
+            "connect-src 'self'",
+            "object-src 'none'",
+            "base-uri 'self'",
+            "form-action 'self'",
+          ],
+
+          styleDirective: {
+            resources: ["'self'", "https://fonts.googleapis.com"],
+          },
+        }
+      : false,
   },
+
   integrations: [sitemap()],
 });
